1cf9d7bfc50c7da45eadd9eaa5a2de5b1a23ea80
nadinejuraschek <nadinejwebdev@gmail.com>
2023-02-19 12:24:03 +0100

    Fix: button styling

--- client/src/pages/sandbox/index.jsx
+++ client/src/pages/sandbox/index.jsx
@@ -1,11 +1,23 @@
-import { Input } from "components";
+import { Button, Input } from "components";
+
 import { useState } from "react";
 import styles from "./sandbox.module.css";
 
 export const Sandbox = () => {
   const [value, setValue] = useState("");
 
-  return <div className={ styles.container }>
-    <Input error="This field is required" name="name" value={ value } handleChange={ (e) => setValue(e.target.value) } label="Name" />
-  </div>;
+  return (
+    <div className={ styles.container }>
+      <Button variant="primary">Primary</Button>
+      <Button disabled variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button disabled variant="secondary">Secondary</Button>
+      <Button variant="tertiary">Tertiary</Button>
+      <Button disabled variant="tertiary">Tertiary</Button>
+      <Button variant="danger">Danger</Button>
+      <Button disabled variant="danger">Danger</Button>
+      <Button variant="warning">Warning</Button>
+      <Button disabled variant="warning">Warning</Button>
+      <Input error="This field is required" name="name" value={ value } handleChange={ (e) => setValue(e.target.value) } label="Name" />
+    </div>);
 }
