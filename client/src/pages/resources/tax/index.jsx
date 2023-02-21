88a8526c2296e388698b184906ab07adbd59549d
nadinejuraschek <nadinejwebdev@gmail.com>
2023-02-21 20:10:14 +0100

    New: taxes resources styling updated

--- client/src/pages/resources/tax/index.jsx
+++ client/src/pages/resources/tax/index.jsx
@@ -1,21 +1,22 @@
+import { Text } from "components";
 import { taxInfo } from "data";
 import { Fragment } from "react";
 import { createMarkup } from "utils";
-import styles from "../resources.module.css";
+import styles from "./tax.module.css";
 
 export const Tax = () => {
   const { contentRows, headerRows } = taxInfo;
 
   const renderHeader = () => headerRows.cells.map( ( cell, index ) => (
     <Fragment key={ index }>
-      <h2 className={ styles.header }>{ cell.title }</h2>
+      <Text as="h3" size="xl" className={ styles.header } weight="bold">{ cell.title }</Text>
       <p className={ styles.info } dangerouslySetInnerHTML={ createMarkup( cell.text ) } />
     </Fragment>
   ) );
 
   const renderContentRows = () => contentRows.cells.map( ( cell, index ) => (
     <div className={ styles.infoItem } key={ index }>
-      <h3>{ cell.title }</h3>
+      <Text as="h3" size="md" weight="bold">{ cell.title }</Text>
       <p dangerouslySetInnerHTML={ createMarkup( cell.text ) } />
     </div>
   ) );
