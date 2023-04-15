1f9bf8a8718d799ceea7d5ead8ba7a6cd7ce9d9b
nadinejuraschek <nadinejwebdev@gmail.com>
2023-04-15 18:29:24 +0200

    New: modal component integrated in add note feature

--- client/src/pages/notebook/notes/AddNoteModal/index.tsx
+++ client/src/pages/notebook/notes/AddNoteModal/index.tsx
@@ -1,15 +1,16 @@
 import * as dayjs from "dayjs";
 
-import { Button, Card, Text } from "components";
+import { Button, Input, Modal } from "components";
 import { ChangeEvent, FormEvent } from 'react';
 import { useContext, useState } from "react";
 
+import { IAddNoteModal } from './types';
 import { NoteContext } from "contexts";
 import axios from "axios";
-import styles from "./addNote.module.css";
+import styles from "./addNoteModal.module.css";
 import { toast } from 'react-toastify';
 
-export const AddNote = (): JSX.Element => {
+export const AddNoteModal = ({ toggleModal }: IAddNoteModal): JSX.Element => {
   const currentDate = dayjs().format("MMMM D, YYYY");
   const { getNotes } = useContext( NoteContext );
   const [newNote, setNewNote] = useState( { date: currentDate, text: "", title: "" } );
@@ -23,36 +24,49 @@ export const AddNote = (): JSX.Element => {
     } )
       .then( () => {
         toast.success('Your note has been added successfully!');
-        // re-render component
-        setNewNote( { date: currentDate, text: "", title: "" } );
         getNotes();
       } )
       .catch( () => {
         toast.error('The note could not be added. Please try again later!');
         // console.debug( "Error when adding a note: " + error );
-      } );
-  };
+      } )
+      .finally(() => {
+        toggleModal();
+        setNewNote( { date: currentDate, text: "", title: "" } );
+      });
+    };
 
   const handleChange = (event: ChangeEvent): void => {
     const target = event.target as HTMLInputElement;
     const name = target.name;
     const value = target.value;
-    setNewNote( newNote => ( { ...newNote, [name]: value } ) );
+    setNewNote( newNote => ( { ...newNote, [name]: value } ) )
   };
 
+  // TODO: handleSubmit in form instead of button
+  const addNoteModalActions = (
+    <>
+      <Button fullWidth handleClick={toggleModal}>Cancel</Button>
+      <Button autoFocus handleClick={handleSubmit} fullWidth type="submit" variant="primary">Save</Button>
+    </>
+  );
+
   return (
-    <Card className={ styles.addNote }>
-      <Text as="h4" size="sm" weight="bold">New Note</Text>
-      <form className="ui form" onSubmit={ handleSubmit }>
-        <div className="field">
-          <input
-            name="title"
-            type="text"
-            onChange={ handleChange }
-            placeholder="Title"
-            value={ newNote.title }
-          />
-        </div>
+    <Modal
+      actions={addNoteModalActions}
+      handleClose={toggleModal}
+      title="New Note"
+    >
+      <form>
+        <Input
+          fullWidth
+          label="Title"
+          name="title"
+          handleChange={ handleChange }
+          placeholder="Title"
+          value={ newNote.title }
+        />
+        {/* TODO: replace with new Textarea component */}
         <div className="field">
           <textarea
             className={ styles.textarea }
@@ -63,10 +77,7 @@ export const AddNote = (): JSX.Element => {
             value={ newNote.text }
           />
         </div>
-        <Button fullWidth type="submit" variant="primary">
-          Add Note
-        </Button>
       </form>
-    </Card>
+    </Modal>
   );
 }
